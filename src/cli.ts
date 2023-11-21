143b3b7a0b9c48594ab913729cb997bdd17a6aa3
Lucas Albuquerque <lucastableless@gmail.com>
2023-11-21 11:09:33 -0300

    feat: polyglotte cli

---
+++ src/cli.ts
@@ -0,0 +1,56 @@
+import * as readline from "readline";
+import generateTranslations from "./index"; // Adjust the import path as needed
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function prompt(question: string): Promise<string> {
+  return new Promise((resolve) => {
+    rl.question(question, (answer) => {
+      resolve(answer.trim());
+    });
+  });
+}
+
+async function main() {
+  const arrayToTranslate = (
+    await prompt("Enter a comma-separated list of phrases to translate:")
+  )
+    .split(",")
+    .map((phrase) => phrase.trim());
+
+  const languages = (
+    await prompt("Enter a comma-separated list of target languages:")
+  )
+    .split(",")
+    .map((lang) => lang.trim());
+
+  const freeApi =
+    (await prompt("Use the free API? (yes/no):")).toLowerCase() === "yes";
+
+  let apiKey = "";
+  if (!freeApi) {
+    apiKey = await prompt("Enter your Google Cloud API Key:");
+  }
+
+  const outputDirectory = await prompt(
+    "Enter the output directory for translations:"
+  );
+
+  // Call the generateTranslations function
+  const translations = generateTranslations(
+    arrayToTranslate,
+    languages,
+    freeApi,
+    apiKey,
+    outputDirectory
+  );
+
+  console.log(translations);
+
+  rl.close();
+}
+
+main();
